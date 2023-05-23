18c3618101425aecc01ce42ae90fb0a56410eb0f
david-soto-morales <134084808+david-soto-morales@users.noreply.github.com>
2023-05-23 10:05:55 -0600

    Update mani.py

--- mani.py
+++ mani.py
@@ -8,3 +8,11 @@ def index():
 @app.get("/tortas/{num}")
 def dav(num):
     return num
+@app.get("/conversor_caf/{C}")
+def temp{C}:
+     try:
+            C=float(C)
+            TF=C*(9/5) + 32
+            return f"la temperatura es de {TF} grados sentigrados"
+    except:
+            return"eso no era"
